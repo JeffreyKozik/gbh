81e86a58bd780e9b0baa7f45cbf191bf2d8a1422
edf42001 <ethandf2@gmail.com>
2019-09-21 14:42:51 -0400

    Add script file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/6.6.1/firebase-auth.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJeOk00rKxqC7vqH12T-OysJ88Y000ZaA",
+    authDomain: "volauthtest.firebaseapp.com",
+    databaseURL: "https://volauthtest.firebaseio.com",
+    projectId: "volauthtest",
+    storageBucket: "",
+    messagingSenderId: "817461452456",
+    appId: "1:817461452456:web:159163e0d1cd3ef3bf6c9d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>

697aa58c3e016516dd4dfd9e7f40498b81f572c5
edf42001 <ethandf2@gmail.com>
2019-09-21 21:48:24 -0400

    Add interests, skills, and causes to main page

--- main-page-script.js
+++ main-page-script.js
@@ -8,10 +8,16 @@ firebase.auth().onAuthStateChanged(function(user) {
         database.ref("users/" + currentUser.uid + "/info").on("value", function(snapshot){
             var data = snapshot.val();
             var name = data.name;
-            var bio = data.bio;
+            var dob = data.dob;
+            var interests = data.interests;
+            var skills = data.skills;
+            var causes = data.causes;
 
             document.querySelector("#name-info").innerHTML = "My name is: " + name;
-            document.querySelector("#bio-info").innerHTML = "About me: " + bio;
+            document.querySelector("#dob-info").innerHTML = "My DOB is: " + dob;
+            document.querySelector("#interests-info").innerHTML = "My interests are: " + interests;
+            document.querySelector("#skills-info").innerHTML = "My skills are: " + skills;
+            document.querySelector("#causes-info").innerHTML = "My causes are: " + causes;
         });
     } else {
       // No user is signed in.
